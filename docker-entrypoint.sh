9308025704467179bb395e159f84b8afdf3268b2
qiujun <i@qiujun.me>
2026-03-06 15:45:45 +0800

    fix: skip group/user creation if already exists on container restart

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 
-addgroup -g "$PGID" app
-adduser -D -u "$PUID" -G app -s /sbin/nologin app
+getent group app >/dev/null 2>&1 || addgroup -g "$PGID" app
+id app >/dev/null 2>&1 || adduser -D -u "$PUID" -G app -s /sbin/nologin app
 chown app:app /data
 
 exec su-exec app "$@"
